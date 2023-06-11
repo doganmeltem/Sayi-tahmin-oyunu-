b67b4d8fe0311051076104ad52ba4ae8fb75dcf8
doganmeltem <97318655+doganmeltem@users.noreply.github.com>
2023-06-11 20:24:52 +0300

    Update sayitahminoyunu.py

--- sayitahminoyunu.py
+++ sayitahminoyunu.py
@@ -1,11 +1,4 @@
-"""
-1-100 arasında rastgele üreticlecek bir sayıyı asagı yukarı ifadelerle buldurmaya calısın 
-(hak =5)
-**"random modulu" için python random seklinde arama yapın
-100 uzerinden puanlama yapın her soru 20 puan 
-hak bilgisini kullanıcıdan alın ve her spru belirtilen can sayısı uzerinden hesaplansın 
 
-"""
 import random
 sayi = random.randint(1,100)
 
